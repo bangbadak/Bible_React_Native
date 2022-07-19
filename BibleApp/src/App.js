43fb66ee77254955fe90e8c0459df5f0828e4915
bangbadak <yosephlee050523@gmail.com>
2022-07-19 12:13:00 +0900

    오류는 많지만 성과는 있음, 번호 입력은 사용 안함

--- BibleApp/src/App.js
+++ BibleApp/src/App.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DisplayChapter from './DisplayChapter';
+import NumericInput from 'react-native-numeric-input';
+
+import {
+  Text,
+  View,
+  ScrollView,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  Pressable,
+  TextInput,
+} from 'react-native';
 
 const App = () => {
-  return <DisplayChapter />;
+  const [chapter, setChapter] = useState(1);
+  const [verses, setVerses] = useState([]);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <TextInput
+          style={headerStyle.input}
+          keyboardType="numeric"
+          placeholder="Input Num"
+          onChangeText={text => {
+            setChapter(parseInt(text));
+          }}
+        />
+        <Button title="Select" onPress={setVerses(DisplayChapter(chapter))} />
+      </View>
+
+      <ScrollView style={headerStyle.text}>{verses}</ScrollView>
+    </SafeAreaView>
+  );
 };
 
+const headerStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {},
+  text: {
+    fontSize: 16,
+    color: 'ffffff',
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+  },
+});
+
 export default App;
+
+// event => {
+//   const {eventCount, target, text} = event.nativeEvent;
+//   setChapter(text);
+//   console.log(chapter);
+// }
--- BibleApp/src/DisplayChapter.js
+++ BibleApp/src/DisplayChapter.js
@@ -1,32 +1,27 @@
 import React, {useEffect, useState, useRef} from 'react';
 import {Text, View, ScrollView, Button, StyleSheeet} from 'react-native';
-import GetChapter from './GetChapter';
 
-const DisplayChapter = () => {
+const DisplayChapter = index => {
+  console.log('DisplayChapter called');
   const [output, setOutput] = useState([]);
+
+  console.log('output delcared');
   useEffect(() => {
     GetChapter();
   }, []);
 
   const GetChapter = async () => {
     try {
-      const response = await fetch('https://bible-api.com/gen+1');
+      const response = await fetch('https://bible-api.com/gen+' + index);
       const json = await response.json();
       for (var i = 0; i < json.verses.length; i++) {
-        setOutput(arr => [...arr, i + 1 + '절: ' + json.verses[i].text]);
+        setOutput(arr => [...arr, i + 1 + '. ' + json.verses[i].text]);
       }
     } catch (e) {
       console.error(e);
     }
   };
-
-  return (
-    <ScrollView>
-      {output.map((text, id) => (
-        <Text key={id}>{text}</Text>
-      ))}
-    </ScrollView>
-  );
+  return output.map((text, id) => <Text key={id}>{text}</Text>);
 };
 
 export default DisplayChapter;
